8a87d83b3a4b9eac1d530ffe40182c3ddc0279a3
Reispfannenfresser <34158734+Reispfannenfresser@users.noreply.github.com>
2022-06-02 22:03:37 +0200

    Fixed camera movement framerate independence.

--- Assets/Scripts/Testing/CameraController.cs
+++ Assets/Scripts/Testing/CameraController.cs
@@ -4,7 +4,7 @@ class CameraController : MonoBehaviour {
 	[SerializeField]
 	float movementSpeed = 10f;
 	[SerializeField]
-	float turnSpeed = 300f;
+	float turnSpeed = 10f;
 	[SerializeField]
 	float runMultiplier = 2.5f;
 
@@ -20,7 +20,7 @@ class CameraController : MonoBehaviour {
 		bool running = Input.GetButton("Fire3");
 		transform.position += horizontal * Time.deltaTime * movementSpeed * (running ? runMultiplier : 1);
 
-		rotation += new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * turnSpeed * Time.deltaTime;
+		rotation += new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * turnSpeed;
 		rotation.x = Mathf.Clamp(rotation.x, -90, 90);
 
 		transform.rotation = Quaternion.Euler(rotation);
